3f0e6eb34b0e0bfea9c57d5bb1af2bac111598f8
Kiagoblin <kiagoblin@gmail.com>
2026-09-21 09:46:14 +0300

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,16 +1,36 @@
 package example;
 
 import arc.util.Log;
+import mindustry.content.Items;
+import mindustry.type.Category;
+import mindustry.world.Block;
+import mindustry.world.blocks.storage.StorageBlock;
 import mindustry.mod.Mod;
 
-public class ExampleJavaMod extends Mod{
+public class ExampleJavaMod extends Mod {
 
-    public ExampleJavaMod(){
-        Log.info("Goblin Storage v0.3.0 loaded!");
+    public static Block goblinLink;
+
+    public ExampleJavaMod() {
+        Log.info("Goblin Storage v0.3.1 loaded!");
     }
 
     @Override
-    public void loadContent(){
-        Log.info("Goblin Storage content loading...");
+    public void loadContent() {
+
+        goblinLink = new GoblinLink("goblin-link");
+
+        goblinLink.localizedName = "Goblin Link";
+        goblinLink.description = "Remote storage link - test version.";
+
+        goblinLink.category = Category.effect;
+
+        goblinLink.requirements = new mindustry.type.ItemStack[]{
+            new mindustry.type.ItemStack(Items.copper, 100),
+            new mindustry.type.ItemStack(Items.lead, 100),
+            new mindustry.type.ItemStack(Items.graphite, 50)
+        };
+
+        Log.info("Goblin Link registered!");
     }
 }
